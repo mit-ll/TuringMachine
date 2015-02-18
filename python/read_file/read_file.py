534eacb6ddf36157fbe514a06511c209a3b57f39
Chad Spensky <cspensky@mit.edu>
2015-02-18 10:44:11 -0500

    Adding an example to read a file from disk.

---
+++ python/read_file/read_file.py
@@ -0,0 +1,20 @@
+"""
+	This is a sample program to read in a file from the disk.
+"""
+# Define our filename
+filename = "tape.txt"
+
+# Open our file
+f = open(filename)
+
+# Loop over every line in the file
+for line in f:
+	# Read value from line
+	value = line.strip()
+
+	# Print value
+	print value
+
+# Close our file
+f.close()
+
