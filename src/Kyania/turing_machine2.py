654d6d9315a5a70891ecacc6a80982c6379e8fdb
Kyania Burke <ktburke@ccscfile.org>
2015-04-01 13:47:47 -0400

    Revised turing machine
    Revised turing machine

--- src/Kyania/turing_machine2.py
+++ src/Kyania/turing_machine2.py
@@ -11,10 +11,28 @@ statedict = {}
 
 f = open(filename)
 
+import pprint 
+import sys
+import os
+sys.path.append("../")
+
+from tmachine.VirtualHardware import VirtualHardware
+tape = ["_","1","1","_","1","1","1","1","1","1","1","_"]
+turing_machine = VirtualHardware(tape_length=50, init = tape)
+running = True
+state = 0
+#Read in the file 
+
+
 
 for line in f:
     value = line.strip()
     valuelist = value.split()
+    if len(valuelist) != 5:
+        print "can't use line, ERROR!"
+        print len(valuelist)
+        continue 
+    
     
     statename = valuelist[0]
     readvalue = valuelist[1]
@@ -29,24 +47,36 @@ for line in f:
     readdict[valuelist[1]]=statefunction
     
     
-import pprint 
-import sys
-import os
-sys.path.append("../")
+read_val = turing_machine.read()
+print read_val
 
-from tmachine.VirtualHardware import VirtualHardware
-tape = [None,1,1,None,1,1,1,1,1,1,None]
-turing_machine = VirtualHardware(tape_length=50, init = tape)
-running = True
-state = 0
-
-    print "state", state
-    print "tape position =", turing_machine.position()
+state = "0"
+while state != "stop":
     read_val = turing_machine.read()
-    print "read_val=", read_val
+    state_function = None
+    read_function = None
+    
+    if state in statedict:
+        read_function = statedict[state]
+    else:
+        print "ERROR: Found an undefind state (%s)" %state
+    #Look up function from read value
+    if read_val in read_function:
+        state_function = read_function[read_val]
+    else:
+        print "ERROR: Got a read value with no definition. (%s)"%read_val
+        break
+#print read_val
+    write_val = state_function["write_value"]
+    turing_machine.write(write_val)
+    move_dir = state_function["move"]
+    if move_dir == "R":
+        turing_machine.moveLeft()
+    else:
+        turing_machine.moveRight()
+    
+    next_state = state_function["next_state"]
+    print read_val
+    print state_function
+    print state
     print turing_machine.position()
-    
-while statename != stop:
-    statefunction = statedict[state][read_val]
-    
-pprint
