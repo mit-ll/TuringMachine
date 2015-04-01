8c6950c3275751a999b4eedfcdc1b08998abe89c
Trevon Bennett <tabennett@ccscfiles.org>
2015-04-01 13:48:04 -0400

    Revised turing machine 2
    revised version of turing machine, allows for use of any data set

--- src/Trevon/turing2.py
+++ src/Trevon/turing2.py
@@ -6,14 +6,24 @@ Created on Wed Mar 25 12:49:01 2015
 """
 filename = "unary addition.turing.py"
 
+from tmachine.VirtualHardware import VirtualHardware
+tape = ["_","1","1","_","1","1","1","1","1","1","1","_"]
+turing_machine = VirtualHardware(tape_length=50,init = tape)
+running = True
+state = 0
+
 statedict = {}
     
-
+#read in file, poplating statedict(first dictionary)
 f = open(filename)
-
+#same as if statments, run touring machine
 for line in f:
     value = line.strip()
     valuelist= value.split()
+    if len(valuelist) != 5:
+        print "Error! I don't like this line"
+        print len(valuelist)
+        continue
 
     statename = valuelist[0]
     readvalue = valuelist[1]
@@ -32,21 +42,54 @@ import sys
 import os
 sys.path.append("../")
 
-from tmachine.VirtualHardware import VirtualHardware
-tape = [None,1,1,None,1,1,1,1,1,1,1,None]
-turing_machine = VirtualHardware(tape_length=50,init = tape)
-running = True
-state = 0
-#while running == True:
-    print "state=",state
-    print "tape position=",turing_machine.position()
+
+    #while running == True:
+
+
+print read_val
+
+state = "0"
+while state != "stop":
     read_val = turing_machine.read()
-    print "read_val=",read_val
-    print turing_machine.position() 
-   
-while statename != stop:
-    statefunction=statedict[state][read_val]
+    state_function = None
+    read_function = None
+
+    # Lookup state in dictionary    
+    if state in statedict:
+        read_function = statedict[state]
+    else:
+        print "ERROR: Found an undefined state (%s)"%state
+        break
+    # Look up function from read value
+    if read_val in read_function:
+        state_function = read_function[read_val]
+    else :
+        print "ERROR: Got a read value with no definition. (%s)"%read_val
+        break
     
-pprint     
+#print read_val
+    write_val = state_function["wv"]
+    turing_machine.write(write_val)
+    move_dir = state_function["mv"]
+    if move_dir == "R":
+        turing_machine.moveLeft()
+    else:
+        turing_machine.moveRight()
+    next_state =statefunction["ns"]
+    print read_val
+    print state_function
+    print state
+    print turing_machine.position()
+    
+    
+    
+    
+    
+        
+    
+        
+    
+
+
         
     
