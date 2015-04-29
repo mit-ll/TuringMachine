238c0ad4ca5cb2951c07755c6c4643b38a498893
Jack Lepird <jack.lepird@gmail.com>
2015-04-29 16:30:53 -0400

    more gui work

--- src/Trevon/o_my_gui.py
+++ src/Trevon/o_my_gui.py
@@ -1,48 +1,69 @@
 from Tkinter import *
 import options_gui
 
+textOptions = dict(font = ("Helvetica", 16))
+
 class GUI:
         def __init__(self,window):
                 self.window = window
+                self.optionsGUI = None
+                self.language = "English"
+                self.speed = "Slow"
                 
-                w = 8
-                h = 4
+                w = 50
+                h =15
                 self.programs = Button(self.window,
                                              text = "Programs",
                                              height = h,
-                                             width = w,)
+                                             width = w,
+                                             **textOptions)
                 self.programs.grid (row = 1, column  = 1) 
                 self.about = Button(self.window,
                                     text = "About",
                                     height = h,
-                                    width = w,)
+                                    width = w,
+                                    **textOptions)
                 self.about.grid (row = 1, column = 2)
                 self.reset = Button(self.window,
                                              text = "Reset",
                                              height = h,
-                                             width = w,)
+                                             width = w,
+                                             command = self.output,
+                                             **textOptions)
                 self.reset.grid (row = 2, column = 1)
                 self.options = Button(self.window,
                                                  text = "Options",
                                                  height = h,
                                                  width = w,
-                                                 command = self.Options)
+                                                 command = self.showOptions,
+                                                 **textOptions)
                 self.options.grid (row = 2, column = 2)
                                           
-        def Options(self):
-            print "hello"
-#            window = Tk()
-#            window.geometry = ("800x500")
-#            window.wm_title("Turing Machine")
-
-            app = options_gui.Options(window)
-
-            window.mainloop()
+        def showOptions(self):
+            self.setState(DISABLED)
+            t = Toplevel(self.window)
+            t.minsize(width = 300, height = 300)
+            self.optionsGUI = options_gui.Options(t,self)
+            t.protocol("WM_DELETE_WINDOW", self.optionsGUI.close)
+            t.mainloop()
+            
+            
+        def output(self):
+            print self.language
+            print self.speed
+        def setState(self, newState):
+            self.programs.config(state = newState)
+            self.about.config(state = newState)
+            self.reset.config(state = newState)
+            self.options.config(state = newState)
+            
         
             
                                              
 window = Tk()
-window.geometry = ("800x500")
+window.resizable(width=FALSE, height = FALSE)
+window.minsize(width = 600, height = 600)
+#window.geometry = ("2500x1600")
 window.wm_title("Turing Machine")
 
 app = GUI(window)
--- src/Trevon/options_gui.py
+++ src/Trevon/options_gui.py
@@ -4,50 +4,52 @@ Created on Wed Apr 08 15:37:23 2015
 
 @author: Div 5 Cyberpatriot
 """
-import o_my_gui
 from Tkinter import *
 
+textOptions = dict(font = ("Helvetica", 16))
+
 class Options:
-        def __init__(self,window):
+        def __init__(self, window, parent):
                 self.window = window
+                self.window.wm_title("Options")
+                self.parent = parent                
                 
-                w = 8
-                h = 4
-                self.programs = Button(self.window,
-                                             text = "Language",
-                                             height = h,
-                                             width = w,)
-                self.programs.grid (row = 1, column  = 1) 
-                self.about = Button(self.window,
-                                    text = "Speed",
-                                    height = h,
-                                    width = w,)
-                self.about.grid (row = 1, column = 2)
+                self.language = StringVar()
+                self.language.set("English")
+                self.speed = StringVar()
+                self.speed.set("Slow")
+                
+                w = 20
+                h = 20
+
+                self.langMenu = OptionMenu(self.window,
+                                           self.language,
+                                           "English",
+                                           "Spanish")
+
+                self.langMenu.config(**textOptions)                             
+                self.langMenu.grid(row = 1, column = 1)
+                
+                self.speedMenu = OptionMenu(self.window,
+                                           self.speed,
+                                           "Slow",
+                                           "Fast",
+                                           **textOptions)
+                self.speedMenu.grid(row = 1, column = 2)
+
+                
                 self.back = Button(self.window,
                                     text = "Back",
                                     height = h,
                                     width = w,
-                                    command = self.main )
+                                    command = self.close,
+                                    **textOptions)
                 self.back.grid (row = 2, column = 2) 
     
-        def main(self):
-            self.window.destroy()
-            window = Tk()
-            window.geometry = ("800x500")
-            window.wm_title("Turing Machine")
-            app = o_my_gui.GUI(window)
-            window.mainloop()
+        def close(self):
+            self.window.destroy() 
+            self.parent.setState(NORMAL)
+            self.parent.language = self.language.get()
+            self.parent.speed = self.speed.get()
             
             
-            
-            
-
-#window = Tk()
-#window.geometry = ("800x500")
-#window.wm_title("Turing Machine")
-#
-#app = MyApp(window)
-#
-#window.mainloop()
-
-#                
