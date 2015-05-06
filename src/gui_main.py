90c71b42114d189913bf509dbfbd5e416d5d3510
Jack Lepird <jack.lepird@gmail.com>
2015-05-06 15:38:15 -0400

    working barebones gui

--- src/gui_main.py
+++ src/gui_main.py
@@ -1,8 +1,12 @@
-from Tkinter import *
+#from PIL import Image, ImageTk # broken for some reason on windows box
+import Tkinter as tk
+import webbrowser
+
 import options_gui
 import run_gui
 
-textOptions = dict(font = ("Helvetica", 16))
+textOptions = dict(font = ("Helvetica", 18),
+                   bg = "lightblue")
 
 class GUI:
         def __init__(self,window):
@@ -14,45 +18,50 @@ class GUI:
                 
                 w = 50
                 h =15
-                self.programs = Button(self.window,
+                self.programs = tk.Button(self.window,
                                              text = "Programs",
                                              height = h,
                                              width = w,
                                              command = self.showRun,
                                              **textOptions)
-                self.programs.grid (row = 1, column  = 1) 
-                self.about = Button(self.window,
+                self.programs.grid (row = 1, column  = 2) 
+                self.about = tk.Button(self.window,
                                     text = "About",
                                     height = h,
                                     width = w,
+                                    command = self.showAbout,
                                     **textOptions)
-                self.about.grid (row = 1, column = 2)
-                self.reset = Button(self.window,
-                                             text = "Reset",
-                                             height = h,
-                                             width = w,
-                                             command = self.output,
-                                             **textOptions)
-                self.reset.grid (row = 2, column = 1)
-                self.options = Button(self.window,
+                self.about.grid (row = 2, column = 1)
+
+                self.options = tk.Button(self.window,
                                                  text = "Options",
                                                  height = h,
                                                  width = w,
                                                  command = self.showOptions,
                                                  **textOptions)
                 self.options.grid (row = 2, column = 2)
+                
+                # doesn't work on windows, but seems to have better support on unix
+#                image = Image.open("LL.png")
+#                photo = ImageTk.PhotoImage(image)
+#                self.ll = tk.Label(self.window, 
+#                                height = h, 
+#                                width = w,
+#                                image = photo
+#                                )
+
                                           
         def showOptions(self):
-            self.setState(DISABLED)
-            t = Toplevel(self.window)
+            self.setState(tk.DISABLED)
+            t = tk.Toplevel(self.window)
             t.minsize(width = 300, height = 150)
             self.optionsGUI = options_gui.Options(t,self)
             t.protocol("WM_DELETE_WINDOW", self.optionsGUI.close)
             t.mainloop()
         
         def showRun(self):
-            self.setState(DISABLED)
-            t = Toplevel(self.window)
+            self.setState(tk.DISABLED)
+            t = tk.Toplevel(self.window)
             t.minsize(width = 600, height = 600)
             self.runGUI = run_gui.Run(t,self)
             t.protocol("WM_DELETE_WINDOW", self.runGUI.close)
@@ -64,15 +73,16 @@ class GUI:
         def setState(self, newState):
             self.programs.config(state = newState)
             self.about.config(state = newState)
-            self.reset.config(state = newState)
             self.options.config(state = newState)
             
-        
+        def showAbout(self):
+            webbrowser.open("foo.html")
+
             
                                              
-window = Tk()
-window.resizable(width=FALSE, height = FALSE)
-window.minsize(width = 600, height = 600)
+window = tk.Tk()
+window.resizable(width=tk.FALSE, height = tk.FALSE)
+window.minsize(width =400, height = 400)
 #window.geometry = ("2500x1600")
 window.wm_title("Turing Machine")
 
--- src/run_gui.py
+++ src/run_gui.py
@@ -5,9 +5,10 @@ Created on Tue May 05 12:57:46 2015
 @author: John
 """
 
-from Tkinter import *
+import Tkinter as tk
 import tmachine.VirtualHardware as hw
 from os import listdir
+import time
 
 
 textOptions = dict(font = ("Helvetica", 16))
@@ -18,7 +19,9 @@ class Run:
         self.parent = parent
         self.speed = self.parent.speed
         self.window.wm_title("Run Turing Program")
+        self.dead = False
         
+        # Determine programs available to us
         files = []
         for f in listdir("turingFiles"):
             if ".turing" in f:
@@ -26,23 +29,156 @@ class Run:
         
         self.available = files
         
-        self.availableLabel = Label(self.window, text = "Program to Run:", **textOptions)
-        self.availableLabel.grid(row = 1, column = 1)
+        # Display avaiable files
+        self.availableLabel = tk.Label(self.window, text = "Program to Run:", **textOptions)
+        self.availableLabel.grid(row = 1, column = 1, columnspan = 2)
         
-        self.program = StringVar()
-        self.availableMenu = OptionMenu(self.window, self.program, *self.available)
-        self.availableMenu.grid(row = 1, column = 2)
+        self.program = tk.StringVar()
+        self.program.set(self.available[0])
+        self.availableMenu = tk.OptionMenu(self.window, self.program, *self.available)
+        self.availableMenu.grid(row = 1, column = 3, columnspan = 2)
         self.availableMenu.config(width = 20)
         
-        self.stateLabel = Label(self.window, text = "State:", **textOptions)
+        # State tracking and display 
+        self.stateLabel = tk.Label(self.window, text = "State:", **textOptions)
         self.stateLabel.grid(row = 2, column = 1)
-        self.state = StringVar()
-        self.state.set("Start")
-        self.stateDisplay = Label(self.window, textvariable = self.state)
-        self.stateDisplay.config(**textOptions)
+        self.state = tk.StringVar()
+        self.state.set("NULL")
+        self.stateDisplay = tk.Label(self.window, textvariable = self.state)
+        self.stateDisplay.config(bg = "grey", **textOptions)
         self.stateDisplay.grid(row = 2, column = 2)
+        
+        # Display of number of steps
+        self.numStepsLabel = tk.Label(self.window, 
+                                      text = "Step Number:",
+                                      **textOptions)
+        self.numStepsLabel.grid(row = 2, column = 3)
+        self.numSteps = tk.StringVar()
+        self.numSteps.set("0")
+        self.numStepsDisplay = tk.Label(self.window,
+                                        textvariable = self.numSteps,
+                                        bg = "grey",
+                                        **textOptions)
+        self.numStepsDisplay.grid(row = 2, column = 4)
+        
+        # Run
+        self.runButton = tk.Button(self.window,
+                                   text = "Run!",
+                                   command = self.run,
+                                   width = 10,
+                                   height = 3,
+                                   **textOptions)
+        self.runButton.grid(column = 5, row = 1, columnspan = 2, rowspan = 2)
+        
+        self.display = tk.Text(self.window)
+        self.display.grid(row = 3, column = 1, rowspan = 4, columnspan = 5)
+        
+        self.exit = tk.Button(self.window,
+                              text = "Exit",
+                              width = 10, 
+                              height = 3,
+                              command = self.close,
+                              **textOptions)
+        self.exit.grid(row = 9, column = 5, columnspan = 2, rowspan = 2)
         
         
     def close(self):
         self.window.destroy() 
-        self.parent.setState(NORMAL)
+        self.parent.setState(tk.NORMAL)
+        self.dead = True
+    def run(self):
+        
+        # Reset from previous run
+        self.numSteps.set("0")
+        self.state.set("NULL")
+        self.display.delete(1.0, tk.END)
+        self.window.update()
+        self.runButton.config(state = tk.DISABLED)
+        
+        filename = "turingFiles/" + self.program.get()
+        
+        f = open(filename)
+        statedict = {}
+        tape = ["_","1","1","_","1","1","1","1","1","1","1","_"]
+        turing_machine = hw.VirtualHardware(tape_length = 50, init = tape)
+        state = 0
+        
+        
+        #Read in the file 
+        for line in f:
+            value = line.strip()
+            valuelist = value.split()
+            if len(valuelist) != 5:
+                print "can't use line, ERROR!"
+                print len(valuelist)
+                continue 
+            
+            
+            statename = valuelist[0]
+#            readvalue = valuelist[1]
+            statefunction = {"write_value":valuelist[2],
+                      "move":valuelist[3],
+                      "next_state":valuelist[4],}
+                      
+            if statename not in statedict:
+                statedict[valuelist[0]] = {}
+                
+            readdict = statedict [valuelist[0]]
+            readdict[valuelist[1]]=statefunction
+            
+            
+        read_val = turing_machine.read()
+        print read_val
+        
+        state = "0"
+        while state != "stop":
+            time.sleep(1)
+            read_val = turing_machine.read()
+            state_function = None
+            read_function = None
+            
+            if state in statedict:
+                read_function = statedict[state]
+            else:
+                print "ERROR: Found an undefind state (%s)" %state
+            #Look up function from read value
+            if read_val in read_function:
+                state_function = read_function[read_val]
+            else:
+                print "ERROR: Got a read value with no definition. (%s)"%read_val
+                break
+            write_val = state_function["write_value"]
+            turing_machine.write(write_val)
+            move_dir = state_function["move"]
+            if move_dir == "R":
+                turing_machine.moveLeft()
+            else:
+                turing_machine.moveRight()
+            
+            oldState = state
+            state = state_function["next_state"]
+            self.state.set(state)
+            self.numSteps.set(str(int(self.numSteps.get()) + 1))
+            
+            self.display.tag_config("highlight", foreground="blue")
+            self.display.tag_config("normal", foreground = "black")
+            
+            self.display.mark_set("matchStart", "1.0")
+            self.display.mark_set("matchEnd","end")            
+            
+            self.display.tag_add("normal", "matchStart", "matchEnd")
+            
+            self.display.insert(tk.END,"State: " + oldState + " Read: " + read_val + " -> Next State: " + state + " Moving: " + move_dir + "\n", "highlight")
+            
+            if state == "stop":
+                self.display.tag_config("done",foreground = "darkgreen")
+                self.display.insert(tk.END,"COMPLETE.\n", "done")            
+            
+            self.window.update()
+
+            turing_machine.tape()
+            
+            # Detect stop
+            if self.dead:
+                break
+        self.runButton.config(state = tk.NORMAL)
