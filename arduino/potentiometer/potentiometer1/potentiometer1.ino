57a86fcfa2b8a73489a8c99798de0535195601c1
Chad Spensky <chad.spensky@ll.mit.edu>
2015-05-27 12:01:36 -0400

    Adding the sketchs for our potentiometers

--- arduino/potentiometer/potentiometer1/potentiometer1.ino
+++ arduino/potentiometer/potentiometer1/potentiometer1.ino
@@ -1,15 +1,5 @@
-/* Analog Read to LED
- * ------------------ 
- *
- * turns on and off a light emitting diode(LED) connected to digital  
- * pin 13. The amount of time the LED will be on and off depends on
- * the value obtained by analogRead(). In the easiest ase we connect
- * a potentiometer to analog pin 2.
- *
- * Created 1 December 2005
- * copyleft 2005 DojoDave <http://www.0j0.org>
- * http://arduino.berlios.de
- *
+/* Potentiometer 1
+ * This is the code for the left potentiometer to maintain tension on the film
  */
 
 #include <Wire.h>
@@ -17,7 +7,6 @@
 #include "utility/Adafruit_PWMServoDriver.h"
 
 int potPin = 2;    // select the input pin for the potentiometer
-int ledPin = 13;   // select the pin for the LED
 int val = 0;       // variable to store the value coming from the sensor
 
 // Create the motor shield object with the default I2C address
@@ -28,25 +17,29 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
 void setup() {
+  
+  // Start our serial
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
   
   AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
-  myMotor->setSpeed(10);  // 10 rpm   
+  myMotor->setSpeed(100);  // 100 rpm   
 }
 
 void loop() {
+  
+  // Read our potentiometer value
   val = analogRead(potPin);    // read the value from the sensor
   Serial.println(val);
   
-  if (val > 800) {
-    myMotor->step(200, FORWARD, DOUBLE); 
-  } else if (val < 200) {
-    myMotor->step(200, BACKWARD, DOUBLE); 
+  // Based on our thresholds, feed or draw in the film
+  if (val > 360) {
+    myMotor->step(1, FORWARD, DOUBLE); 
+  } else if (val < 350) {
+    myMotor->step(1, BACKWARD, DOUBLE); 
   }
 }
- 
+
---
+++ arduino/potentiometer/potentiometer2/potentiometer2.ino
@@ -0,0 +1,46 @@
+/* Potentiometer 2
+ * This is the code for the right potentiometer to maintain tension on the film
+ */
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
+int potPin = 2;    // select the input pin for the potentiometer
+int val = 0;       // variable to store the value coming from the sensor
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
+void setup() {
+  
+  // Setup our serial
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  
+  myMotor->setSpeed(100);  // 100 rpm   
+}
+
+void loop() {
+
+  // Read our potentiometer value
+  val = analogRead(potPin);    // read the value from the sensor
+  Serial.println(val);
+  
+  // Based on our thresholds, feed or draw in the film
+  if (val > 295) {
+    myMotor->step(1, FORWARD, DOUBLE); 
+  } else if (val < 285) {
+    myMotor->step(1, BACKWARD, DOUBLE); 
+  }
+}
+
+
