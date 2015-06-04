6a58a19860e2e4d46cdad4c0a66a53f918826723
Chad Spensky <cspensky@gmail.com>
2015-06-04 13:51:19 -0400

    Update main_gantry.ino

--- arduino/main_gantry/main_gantry.ino
+++ arduino/main_gantry/main_gantry.ino
@@ -1,15 +1,5 @@
-/* Analog Read to LED
- * ------------------ 
- *
- * turns on and off a light emitting diode(LED) connected to digital  
- * pin 13. The amount of time the LED will be on and off depends on
- * the value obtained by analogRead(). In the easiest ase we connect
- * a potentiometer to analog pin 2.
- *
- * Created 1 December 2005
- * copyleft 2005 DojoDave <http://www.0j0.org>
- * http://arduino.berlios.de
- *
+/* 
+ * This Arduino drives all of the motors on the turing machine using a motor shield.
  */
 
 #include <Wire.h>
